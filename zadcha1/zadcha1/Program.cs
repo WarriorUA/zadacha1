11ea05d2b21a918cf56321763b61c27412ed7749
WarriorUA <dinozawrua@gmail.com>
2016-12-04 18:49:24 +0200

    Add files via upload

---
+++ zadcha1/zadcha1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadcha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0) Console.WriteLine("Dodatne");
+            else if (n < 0) Console.WriteLine("Widemne");
+            else Console.WriteLine("NULL");
+            Console.ReadKey();
+        }
+    }
+}
